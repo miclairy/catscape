e7d813b5219ae71438704d0c0c2792675714906c
claire <cba62@uclive.ac.nz>
2016-08-28 21:10:28 +1200

    loads kittens on load floats to left

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,51 +1,47 @@
-$('#search').click(getPhotoRequest("kitten"));
-    
+
 function getPhotoRequest(input){
-    console.log("api call");
+    console.log("api call", input);
     $.ajax({
-    url: "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=a912268eed1f9b50f37faf7b243ab7e4&tags=" + input + "%2Ccute&tag_mode=all&sort=relevance&content_type=1&format=json&nojsoncallback=1&auth_token=72157669879953884-976245242135a923&api_sig=7be241c3bbc2c0c80110266b6e3db2a8",
+    url: "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=a912268eed1f9b50f37faf7b243ab7e4&tags=" + input + "%2Ccute&tag_mode=all&sort=relevance&content_type=1&format=json&nojsoncallback=1",
     type: "GET"
     })
     
     .done(function (data){
-        if (data.length != 0){
-            console.log("photo data search" , data.photos.photo[0]);
-  
+        if (data.stat != "fail"){
+            console.log("photo data search" , data);
+            var photos = new Array();
             $.each(data.photos.photo, function(i, data){
                 //owner = data.photos.photo[i].owner;
                 //id = data.photos.photo[i].id;
-                console.log("i, data in add photo ",i, data.id);
+                photoUrl = "https://www.flickr.com/photos/" + data.owner + "/" + data.id
+                photos.push(photoUrl);
+                console.log("i, data in add photo ",i, data.id, photoUrl);
                 sizesUrl = "https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=a912268eed1f9b50f37faf7b243ab7e4&photo_id="+ data.id +"&format=json&nojsoncallback=1"; 
                 $.getJSON(sizesUrl, function(size){
-                    console.log("grab size", size);
-                    $.each(size.sizes.size, function(i, sizeResults){
+                    console.log("grab size", size, photos[i]);
+                    $.each(size.sizes.size, function(j, sizeResults){
                         if (sizeResults.label == "Large Square"){
-                            $("#photo").append('<p><a href="' + sizeResults.url + '"><img src="' + sizeResults.source + '"/></a></p>');  
+                            $("#photo").append('<p><a href="' + photos[i] + '"><img class=photo src="' + sizeResults.source + '"/></a></p>');  
                         }
                     })
                 })
-
-});
+            });
 
         } else{
+            $("#photo").append('<h3>No cute photos matched :(</h3>');
             console.log("couldn't get data");
         }
     });
 
 }
 
-function addPhoto(i, data){
-    //owner = data.photos.photo[i].owner;
-    id = data.photos.photo[i].id;
-    console.log("i, data in add photo ",i, data);
-    sizesUrl = "https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=a912268eed1f9b50f37faf7b243ab7e4&photo_id=" + data.id + "&format=json&nojsoncallback=1&auth_token=72157669879953884-976245242135a923&api_sig=e54875c47a3c05c2e81009436f78f312"
-    $.getJSON(sizesUrl, function(size){
-        console.log("grab size", size.sizes);
-        $.each(size.sizes.size, function(i, sizeResults){
-            if (sizeResults.label == "Large Square"){
-                $("#photo").append('<p><a href="' + sizeResults.url + '"><img src="' + sizeResults.source + '"/></a></p>');  
-            }
-        });
-    });
+$(document).ready(function(){
+    //console.log($("#searchBox").text());
+    getPhotoRequest("kitten");
+    input = document.getElementById("searchBox").value;
+    alert(input);
+    getPhotoRequest(input);
+    $("#search").click(getPhotoRequest());
+   
+}); 
 
-}
