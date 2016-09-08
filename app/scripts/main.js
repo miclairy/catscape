68dd823397d79f69bd0c3e5e13fc47286f062f66
claire <cba62@uclive.ac.nz>
2016-09-08 22:41:46 +1200

    hover with titles works

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -21,11 +21,11 @@ function getPhotoRequest(input) {
                 //id = data.photos.photo[i].id;
                 var photoUrl = "https://www.flickr.com/photos/" + data.owner + "/" + data.id;
                 photos.push(photoUrl);
-                console.log("i, data in add photo ", i, data.id, photoUrl);
+                //console.log("i, data in add photo ",i, data.id, photoUrl);
                 var id = data.id;
                 var photoInfoUrl = "https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=09bba6751d0e8f198473cd4fc6d8512f&photo_id=" + id + "&format=json&nojsoncallback=1";
                 $.getJSON(photoInfoUrl, function (info) {
-                    console.log("info on photo", info.photo.title._content);
+                    //console.log("info on photo", info.photo.title._content);
                     title = info.photo.title._content;
                     titles.push(title);
                     author = info.photo.owner.username;
@@ -37,7 +37,28 @@ function getPhotoRequest(input) {
                             if (sizeResults.label == "Large Square") {
                                 console.log(count, count % 6, j, sizeResults.source);
                                 //  if (count%6 == 0){
-                                $("#photo").append('<li class="col-lg-2 col-md-2 col-sm-3 col-xs-4 hov"><div class="caption"><p>"' + titles[i] + '"</p></div><img src="' + sizeResults.source + '" id:"' + i + '" class=photo/></li>');
+                                $("#photos").append('<li class="col-lg-2 col-md-2 col-sm-3 col-xs-4 hov "><div class="caption"><p>"' + titles[i] + '"</p></div><img class="photo" src="' + sizeResults.source + '" id:"' + i + '"/></li>');
+                                console.log($(".photo").attr("id"));
+                                $('.hov').on('click', function () {
+                                    var src = $(this).attr('src');
+                                    var img = '<img src="' + sizeResults.source + '" class="img-responsive"/>';
+                                    var modal = $('#myModal');
+                                    modal.modal();
+                                    console.log($(this));
+                                    var id = $(this).attr('class');
+                                    $(".modal-body").html(img);
+                                    // $('#myModal').on('shown.bs.modal', function(){
+                                    //     $('#myModal .modal-body').html(img);
+                                    // });
+                                    // $('#myModal').on('hidden.bs.modal', function(){
+                                    //     $('#myModal .modal-body').html('');
+                                    // });
+                                });
+                                $('.hov').hover(function () {
+                                    $(this).find('.caption').show();
+                                }, function () {
+                                    $(this).find('.caption').hide();
+                                });
                                 // } else {
                                 //     $("#photo").append('<div class="col-sm-2"><p><a href=""><img class=photo src="' + sizeResults.source + '"/></a></p></div>');
                                 // }
@@ -52,7 +73,7 @@ function getPhotoRequest(input) {
             });
         }
         else {
-            $("#photo").append('<h3>No cute photos matched :(</h3>');
+            $("#photos").append('<h3>No cute photos matched :(</h3>');
             console.log("couldn't get data", data);
         }
     });
@@ -60,6 +81,10 @@ function getPhotoRequest(input) {
 $(document).ready(function () {
     //console.log($("#searchBox").text());
     getPhotoRequest("kitten");
+    var url = "https://farm9.staticflickr.com/8188/28622911343_1982a4293b_q.jpg";
+    var id = 1;
+    var title = "cats are cute aye?";
+    $("#photos").append('<li class="col-lg-2 col-md-2 col-sm-3 col-xs-4 hov photo"><div class="caption"><p>"' + title + '"</p></div><img src="' + url + '" id:"' + 1 + '"/></li>');
     //$(".photo").remove();
     //alert('done');
     // getPhotoRequest("owl");
@@ -70,39 +95,30 @@ $(document).ready(function () {
         //alert(input); 
         getPhotoRequest(input);
     });
-    // $('body').waitForImages().done(function() {
-    //     // All descendant images have loaded, now slide up.
-    //     alert("loaded");
-    // });
     $('.hov').hover(function () {
         $(this).find('.caption').show();
     }, function () {
         $(this).find('.caption').hide();
     });
-    $('.static').on('click', function () {
-        alert("you clicked the photo woop");
+    $("img").on('click', function () {
+        //alert("you clicked the static photo woop");
         var src = $(this).attr('src');
-        var img = '<img src="https://farm3.staticflickr.com/2854/9972740276_66b3c326f1_q.jpg" class="img-responsive"/>';
-        var modal = $('#myModal');
+        var img = '<img src="' + src + '" class="img-responsive"/>';
+        var modal = $('#fullSizePhoto');
         modal.modal();
-        $('#myModal').on('shown.bs.modal', function () {
-            $('#myModal .modal-body').html(img);
-        });
-        $('#myModal').on('hidden.bs.modal', function () {
-            $('#myModal .modal-body').html('');
-        });
+        $("#fill").html(img);
     });
-    $('.photo').on('click', function () {
-        alert("you clicked the photo woop");
-        var src = $(this).attr('src');
-        var img = '<img src="https://farm3.staticflickr.com/2854/9972740276_66b3c326f1_q.jpg" class="img-responsive"/>';
-        var modal = $('#myModal');
-        modal.modal();
-        $('#myModal').on('shown.bs.modal', function () {
-            $('#myModal .modal-body').html(img);
-        });
-        $('#myModal').on('hidden.bs.modal', function () {
-            $('#myModal .modal-body').html('');
-        });
-    });
+    // $('.photo').on('click',function(){
+    //     alert("you clicked the photo woop");
+    //     var src = $(this).attr('src');
+    //     var img = '<img src="https://farm3.staticflickr.com/2854/9972740276_66b3c326f1_q.jpg" class="img-responsive"/>';
+    //     var modal:any = $('#myModal')
+    //     modal.modal();
+    //     $('#myModal').on('shown.bs.modal', function(){
+    //         $('#myModal .modal-body').html(img);
+    //     });
+    //     $('#myModal').on('hidden.bs.modal', function(){
+    //         $('#myModal .modal-body').html('');
+    //     });
+    // });
 });
--- app/scripts/main.ts
+++ app/scripts/main.ts
@@ -23,16 +23,17 @@ function getPhotoRequest(input:String) : void{
                 //id = data.photos.photo[i].id;
                 var photoUrl:string = "https://www.flickr.com/photos/" + data.owner + "/" + data.id
                 photos.push(photoUrl);
-                console.log("i, data in add photo ",i, data.id, photoUrl);
-                var id:number = data.id;
+                //console.log("i, data in add photo ",i, data.id, photoUrl);
+                var id:number = data.id;  
                 var photoInfoUrl:string = "https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=09bba6751d0e8f198473cd4fc6d8512f&photo_id="+ id +"&format=json&nojsoncallback=1"; 
                 $.getJSON(photoInfoUrl, function(info:any): void{
-                    console.log("info on photo", info.photo.title._content);
+                    //console.log("info on photo", info.photo.title._content);
                     title = info.photo.title._content;
                     titles.push(title);
                     author = info.photo.owner.username;
                     var sizesUrl:string = "https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=09bba6751d0e8f198473cd4fc6d8512f&photo_id="+ data.id +"&format=json&nojsoncallback=1"; 
                     $.getJSON(sizesUrl, function(size:any) : void{
+
                     // console.log("grab size", size, photos[i]);
                         $.each(size.sizes.size, function(j:number, sizeResults:any) : void{
                         //console.log(sizeResults.label); 
@@ -40,33 +41,77 @@ function getPhotoRequest(input:String) : void{
                             
                                 console.log(count, count%6,j, sizeResults.source);                           
                             //  if (count%6 == 0){
-                                    $("#photo").append('<li class="col-lg-2 col-md-2 col-sm-3 col-xs-4 hov"><div class="caption"><p>"' + titles[i] +'"</p></div><img src="' + sizeResults.source + '" id:"' + i + '" class=photo/></li>');
+                                    
+                                    $("#photos").append('<li class="col-lg-2 col-md-2 col-sm-3 col-xs-4 hov "><div class="caption"><p>"' + titles[i] +'"</p></div><img class="photo" src="' + sizeResults.source + '" id:"' + i + '"/></li>');
+                                    console.log($(".photo").attr("id")) ; 
+                                    $('.hov').on('click',function(){
+                                        var src = $(this).attr('src');
+                                        var img = '<img src="'  + sizeResults.source +  '" class="img-responsive"/>';
+                                        var modal:any = $('#myModal')
+                                        modal.modal();
+                                         console.log($(this)) ; 
+                                        var id = $(this).attr('class');
+                                        $(".modal-body").html(img);
+                                        // $('#myModal').on('shown.bs.modal', function(){
+                                        //     $('#myModal .modal-body').html(img);
+                                        // });
+                                        // $('#myModal').on('hidden.bs.modal', function(){
+                                        //     $('#myModal .modal-body').html('');
+                                        // });
+                                    });
+
+                                        $('.hov').hover(
+                                            function(){
+                                                $(this).find('.caption').show();
+                                            },
+                                            function(){
+                                                $(this).find('.caption').hide();
+                                            }
+                                        );
+
                                 // } else {
                                 //     $("#photo").append('<div class="col-sm-2"><p><a href=""><img class=photo src="' + sizeResults.source + '"/></a></p></div>');
                                 // }
+
                                 count = count + 1;
                             }
                             // if (sizeResults.label == "Original"){
                             //     console.log(sizeResults.source);
                             // }
+
                     })
+
                     })
-                })
+
+                })      
+                                                                        
             }); 
 
         } else{
-            $("#photo").append('<h3>No cute photos matched :(</h3>');
+            $("#photos").append('<h3>No cute photos matched :(</h3>');
             console.log("couldn't get data", data);
         }
 
+
+   
+
     });
+
     
 }
 
 $(document).ready(function(){
     //console.log($("#searchBox").text());
     
+
+
+
     getPhotoRequest("kitten");
+
+    var url:String = "https://farm9.staticflickr.com/8188/28622911343_1982a4293b_q.jpg";
+var id:number = 1;
+var title:string = "cats are cute aye?"
+$("#photos").append('<li class="col-lg-2 col-md-2 col-sm-3 col-xs-4 hov photo"><div class="caption"><p>"' + title +'"</p></div><img src="' + url + '" id:"' + 1 + '"/></li>');
 
     //$(".photo").remove();
     //alert('done');
@@ -78,46 +123,38 @@ $(document).ready(function(){
         //alert(input); 
         getPhotoRequest(input);
     });
-        // $('body').waitForImages().done(function() {
-        //     // All descendant images have loaded, now slide up.
-        //     alert("loaded");
-        // });
-        $('.hov').hover(
-            function(){
-                $(this).find('.caption').show();
-            },
-            function(){
-                $(this).find('.caption').hide();
-            }
-	    );
-   
-        $('.static').on('click',function(){
-            alert("you clicked the photo woop");
-            var src = $(this).attr('src');
-            var img = '<img src="https://farm3.staticflickr.com/2854/9972740276_66b3c326f1_q.jpg" class="img-responsive"/>';
-            var modal:any = $('#myModal')
-            modal.modal();
-            $('#myModal').on('shown.bs.modal', function(){
-                $('#myModal .modal-body').html(img);
-            });
-            $('#myModal').on('hidden.bs.modal', function(){
-                $('#myModal .modal-body').html('');
-            });
-        });
 
-        $('.photo').on('click',function(){
-            alert("you clicked the photo woop");
-            var src = $(this).attr('src');
-            var img = '<img src="https://farm3.staticflickr.com/2854/9972740276_66b3c326f1_q.jpg" class="img-responsive"/>';
-            var modal:any = $('#myModal')
-            modal.modal();
-            $('#myModal').on('shown.bs.modal', function(){
-                $('#myModal .modal-body').html(img);
-            });
-            $('#myModal').on('hidden.bs.modal', function(){
-                $('#myModal .modal-body').html('');
-            });
-        });
+    $('.hov').hover(
+        function(){
+            $(this).find('.caption').show();
+        },
+        function(){
+            $(this).find('.caption').hide();
+        }
+    );
+   
+    $("img").on('click',function(){
+        //alert("you clicked the static photo woop");
+        var src = $(this).attr('src');
+        var img = '<img src="'+ src +'" class="img-responsive"/>';
+        var modal:any = $('#fullSizePhoto')
+        modal.modal();
+        $("#fill").html(img);
+    });
+
+    // $('.photo').on('click',function(){
+    //     alert("you clicked the photo woop");
+    //     var src = $(this).attr('src');
+    //     var img = '<img src="https://farm3.staticflickr.com/2854/9972740276_66b3c326f1_q.jpg" class="img-responsive"/>';
+    //     var modal:any = $('#myModal')
+    //     modal.modal();
+    //     $('#myModal').on('shown.bs.modal', function(){
+    //         $('#myModal .modal-body').html(img);
+    //     });
+    //     $('#myModal').on('hidden.bs.modal', function(){
+    //         $('#myModal .modal-body').html('');
+    //     });
+    // });
 
 
 
